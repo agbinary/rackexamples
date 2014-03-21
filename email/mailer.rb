2755554288ccd43fdaa9e81c22f7a35ff8d121d2
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-03-21 00:04:05 -0500

    Cosmetic changes

--- email/mailer.rb
+++ email/mailer.rb
@@ -4,25 +4,25 @@ require 'httparty'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 class Mailer
-  def initialize (from, to, subject)
-    @from=from
-    @to=to
-    @subject=subject
+  def initialize(from, to)
+    @from = from
+    @to = to
+    @subject = subject
   end
-  def send_email (message)
+
+  def send_email(subject, message)
     url = "https://sendgrid.com/api/mail.send.json"
     response = HTTParty.post url, :body => {
         "api_user" => ENV["SENDGRID_USER"],
         "api_key" => ENV["SENDGRID_TOKEN"],
         "to" => @to,
         "from" => @from,
-        "subject" => @subject,
+        "subject" => subject,
         "html" => message
     }
     response.body
-    puts "Enviado"
   end
 end
 
-mailer= Mailer.new("ang3l_gu@hotmail.com", "ang3l_gu@hotmail.com", "Hola")
+mailer = Mailer.new("ang3l_gu@hotmail.com", "ang3l_gu@hotmail.com", "Hola")
 mailer.send_email("Hola Angie! :D")
