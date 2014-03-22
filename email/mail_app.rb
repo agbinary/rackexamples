6bbb8a9ec269663b70bc4d69f4b9626e0f12836d
Angela Guette <ang3l_gu@hotmail.com>
2014-03-22 17:10:42 -0500

    Update mail_app.rb

--- email/mail_app.rb
+++ email/mail_app.rb
@@ -14,13 +14,8 @@ class MailApp
           case env["PATH_INFO"]
             when "/send"
               form = Rack::Utils.parse_nested_query(env["rack.input"].read)
-<<<<<<< HEAD
               mailer = Mailer.new(form["from_name"], form["from"], form["to"])
               if mailer.send_email(form["subject"], form["message"])
-=======
-              mailer = Mailer.new("Guillermo Iguaran", "guilleiguaran@gmail.com", "ang3l_gu@hotmail.com")
-              if mailer.send_email("Hola", "Hola Angie!")
->>>>>>> 556c7888f43a50f1000dfb7536a460eacb612a23
                 return render "sent.html"
               else
                 return render "error.html"
