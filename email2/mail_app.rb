2bb316e4212b72ba7f276ce604f961b2d61c69f1
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-03-23 22:07:43 -0500

    Use Rack::Request

--- email2/mail_app.rb
+++ email2/mail_app.rb
@@ -3,23 +3,22 @@ require './mailer'
 
 class MailApp
   def call(env)
-    case env["REQUEST_METHOD"]
-      when "GET"
-        case env["PATH_INFO"]
-          when "/"
-            return render "form.html"
-        end
-      when "POST"
-        case env["PATH_INFO"]
-          when "/send"
-            form = Rack::Utils.parse_nested_query(env["rack.input"].read)
-            mailer = Mailer.new(form["from_name"], form["from"], form["to"])
-            if mailer.send_email(form["subject"], form["message"])
-              return render "sent.html"
-            else
-              return render "error.html"
-            end
-        end
+    request = Rack::Request.new(env)
+    if request.get?
+      case env["PATH_INFO"]
+        when "/"
+          return render "form.html"
+      end
+    elsif request.post?
+      case env["PATH_INFO"]
+        when "/send"
+          mailer = Mailer.new(request.params["from_name"], request.params["from"], request.params["to"])
+          if mailer.send_email(request.params["subject"], request.params["message"])
+            return render "sent.html"
+          else
+            return render "error.html"
+          end
+      end
     end
     [404, {"Content-Type" => "text/plain"}, ["Error 404: Page not found"]]
   end
