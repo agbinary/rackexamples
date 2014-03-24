bcff3330c757c1f562b4e26063c45924c2167bd1
angela4713 <ang3l_gu@hotmail.com>
2014-03-23 20:55:42 -0500

    add mail_app_test

---
+++ email/mail_app_test.rb
@@ -0,0 +1,41 @@
+require './mail_app'
+require 'test/unit'
+require 'rack/test'
+require "mocha/mini_test"
+
+class MailAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    MailApp.new
+  end
+
+  def test_request_method_get
+    get "/"
+
+    assert_equal "http://example.org/", last_request.url
+    assert last_response.ok?
+  end
+
+  def test_request_method_post_sent
+    Mailer.any_instance.expects(:send_email).returns(true)
+    post "/send", {:fromname => "Angela", :from => "blabla@gmail.com", :to => "blabla@gmail.com", :subject => "Hola", :message => "Hola Angie!"}
+
+    assert_equal "http://example.org/send", last_request.url
+    assert_match /Email sent/, last_response.body
+    assert last_response.ok?
+
+  end
+
+  def test_request_method_post_error
+    Mailer.any_instance.expects(:send_email).returns(false)
+    post "/send", {:fromname => "Angela", :from => "blabla@gmail.com", :to => "blabla@gmail.com", :subject => "Hola", :message => "Hola Angie!"}
+
+    assert_equal "http://example.org/send", last_request.url
+    assert_match /ERROR!/, last_response.body
+    assert last_response.ok?
+
+  end
+
+
+end
