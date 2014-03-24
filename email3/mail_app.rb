7db975f3b08bbf15b76768531449fcd5e8cb5c12
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-03-24 00:02:11 -0500

    Add some spaces

--- email3/mail_app.rb
+++ email3/mail_app.rb
@@ -1,12 +1,14 @@
 require 'rack/utils'
-require './mailer'
 require 'nancy'
+require './mailer'
 
 class MailApp < Nancy::Base
   include Nancy::Render
+
   get "/" do
     render "form.html"
   end
+
   post "/send" do
     mailer = Mailer.new(params["from_name"], params["from"], params["to"])
     if mailer.send_email(params["subject"], params["message"])
